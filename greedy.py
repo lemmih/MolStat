ad56b1bf02ac5ac0208e7914059680700bdfb486
David Himmelstrup <lemmih@gmail.com>
2017-06-16 13:44:14 +0200

    Update greedy algorithm.

--- greedy.py
+++ greedy.py
@@ -3,22 +3,28 @@ import molecule as mol
 import numpy as np
 import math
 
-no_generations = 100
+no_generations = 20*100
 
 def greedy_optimize(alkane_size):
     mol.generate_chain(alkane_size)
     no_dihedral = alkane_size-3
-    dihedral_list = np.random.uniform(0.0, 360.0, no_dihedral)
+    if no_dihedral == 1:
+        dihedral_list = np.random.uniform(0.0, 180.0, no_dihedral)
+    else:
+        dihedral_list = np.random.uniform(0.0, 360.0, no_dihedral)
     mol.set_dihedral(dihedral_list)
     energy = mol.get_energy()
     for _ in range(no_generations):
-        new_list = np.random.uniform(0.0, 360.0, no_dihedral)
+        if no_dihedral == 1:
+            new_list = np.random.uniform(0.0, 180.0, no_dihedral)
+        else:
+            new_list = np.random.uniform(0.0, 360.0, no_dihedral)
         mol.set_dihedral(new_list)
         new_energy = mol.get_energy()
         if new_energy < energy:
             dihedral_list = new_list
             energy = new_energy
-    print 'Optimized configuration:', energy, dihedral_list
+    print 'Optimized configuration:', alkane_size, energy, dihedral_list[0]
 
 greedy_optimize(4)
 greedy_optimize(10)
